ed5dfc5ba81795347a23f382646e2018bad7b2ad
Leonardo Silva <leosilvadev@gmail.com>
2021-02-21 09:44:48 +0100

    add fake camera

---
+++ IntroToKafka/VehicleDetectorJava/src/main/java/com/github/leosilvadev/vehicle_detector/services/Camera.java
@@ -0,0 +1,5 @@
+package com.github.leosilvadev.vehicle_detector.services;
+
+public interface Camera {
+    byte[] takePicture();
+}
---
+++ IntroToKafka/VehicleDetectorJava/src/main/java/com/github/leosilvadev/vehicle_detector/services/FakeCamera.java
@@ -0,0 +1,22 @@
+package com.github.leosilvadev.vehicle_detector.services;
+
+import java.util.Random;
+
+public class FakeCamera implements Camera {
+
+    private final Random random = new Random();
+
+    public byte[] takePicture() {
+        try {
+            final var is = getClass().getClassLoader().getResourceAsStream(randomFileName());
+            return is.readAllBytes();
+        } catch (final Exception e) {
+            return new byte[]{};
+        }
+    }
+
+    private String randomFileName() {
+        return "fake_detections/detection_" + (random.nextInt(2) + 1) + ".jpg";
+    }
+
+}
