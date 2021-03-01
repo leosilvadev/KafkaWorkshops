3d5d8da89d6d89f79f3b99ef86442156e27b266a
Leonardo Silva <leosilvadev@gmail.com>
2021-03-01 23:08:12 +0100

    temperature consumer

--- IntroToKafka/TemperatureApp/src/main/java/com/github/leosilvadev/temperature_producer/MainConsumer.java
+++ IntroToKafka/TemperatureApp/src/main/java/com/github/leosilvadev/temperature_producer/MainConsumer.java
@@ -1,7 +1,33 @@
 package com.github.leosilvadev.temperature_producer;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.DoubleDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Properties;
+import java.util.UUID;
+
+import static java.util.Collections.singletonList;
+
 public class MainConsumer {
     public static void main(final String[] args) {
+        final var sensorId = UUID.randomUUID().toString();
 
+        final var config = new Properties();
+        config.put(ConsumerConfig.CLIENT_ID_CONFIG, "sensor-" + sensorId);
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, DoubleDeserializer.class);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "sensors-central");
+
+        final var consumer = new KafkaConsumer<String, Double>(config);
+        consumer.subscribe(singletonList("temperatures"));
+        while (true) {
+            consumer.poll(Duration.ofSeconds(10)).forEach(record -> {
+                System.out.printf("Temperature %s received from sensor %s\n", record.key(), record.value());
+            });
+        }
     }
 }
